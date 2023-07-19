ea30cf29c6de9dcb867bce2dda04da7af23cd7ba
jusipat <justin10patrick@gmail.com>
2023-07-19 20:16:30 -0300

    - Added display message popup - Fixed en_us.json localisation

--- src/main/java/com/jprik/nodirtwarfare/CheckSpawn.java
+++ src/main/java/com/jprik/nodirtwarfare/CheckSpawn.java
@@ -2,8 +2,6 @@ package com.jprik.nodirtwarfare;
 
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.world.level.Level;
 import net.minecraftforge.event.entity.player.AttackEntityEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -16,8 +14,9 @@ public class CheckSpawn {
             if (event.getTarget() instanceof ServerPlayer && event.getEntity() instanceof ServerPlayer) {
                 ServerPlayer attacker = (ServerPlayer) event.getEntity();
 
-                // Check if the attacker has a respawn point set
+                // Check if the attacker has a respawn point set and if not, cancel the attack
                 if (attacker.getRespawnPosition() == null) {
+                    attacker.displayClientMessage(Component.translatable("event.nodirtwarfare.warnmessage"), true);
                     event.setCanceled(true);
 
                 }
