3bb90893d51c72b71d89e4d5a7b1388af37a9594
jusipat <justin10patrick@gmail.com>
2023-07-19 18:43:49 -0300

    - Added onAttackEntity event - Updated mods.toml description - Added en_us.json localisation

--- src/main/java/com/jprik/nodirtwarfare/CheckSpawn.java
+++ src/main/java/com/jprik/nodirtwarfare/CheckSpawn.java
@@ -1,7 +1,10 @@
 package com.jprik.nodirtwarfare;
 
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.entity.player.AttackEntityEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -9,10 +12,15 @@ public class CheckSpawn {
     @Mod.EventBusSubscriber(modid = NoDirtWarfare.MOD_ID)
     public static class ForgeEvents {
         @SubscribeEvent
-        public static void checkSpawnPoint(PlayerEvent event) {
-            ServerPlayer player = (ServerPlayer) event.getEntity();
-            if (player.getRespawnPosition() == event.getEntity().getLevel().getSharedSpawnPos()) {
-                
+        public static void onAttackEntity(AttackEntityEvent event) {
+            if (event.getTarget() instanceof ServerPlayer && event.getEntity() instanceof ServerPlayer) {
+                ServerPlayer attacker = (ServerPlayer) event.getEntity();
+
+                // Check if the attacker has a respawn point set
+                if (attacker.getRespawnPosition() == null) {
+                    event.setCanceled(true);
+
+                }
             }
         }
     }
