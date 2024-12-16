59c0a58ab4a742e76da4b23d8b7f19a4af907cdb
jusipat <justin10patrick@gmail.com>
2024-12-15 22:01:27 -0500

    Setup

---
+++ src/main/java/com/jusipat/ndw/MyMod.java
@@ -0,0 +1,38 @@
+package com.jusipat.ndw;
+
+import java.util.logging.Logger;
+
+@Mod(modid = MyMod.MODID, version = Tags.VERSION, name = "NoDirtWarfare", acceptedMinecraftVersions = "[1.7.10]")
+public class MyMod {
+
+    public static final String MODID = "ndw";
+    public static final Logger LOG = LogManager.getLogger(MODID);
+
+    @SidedProxy(clientSide = "com.jusipat.ndw.ClientProxy", serverSide = "com.jusipat.ndw.CommonProxy")
+    public static CommonProxy proxy;
+
+    @Mod.EventHandler
+    // preInit "Run before anything else. Read your config, create blocks, items, etc, and register them with the
+    // GameRegistry." (Remove if not needed)
+    public void preInit(FMLPreInitializationEvent event) {
+        proxy.preInit(event);
+    }
+
+    @Mod.EventHandler
+    // load "Do your mod setup. Build whatever data structures you care about. Register recipes." (Remove if not needed)
+    public void init(FMLInitializationEvent event) {
+        proxy.init(event);
+    }
+
+    @Mod.EventHandler
+    // postInit "Handle interaction with other mods, complete your setup based on this." (Remove if not needed)
+    public void postInit(FMLPostInitializationEvent event) {
+        proxy.postInit(event);
+    }
+
+    @Mod.EventHandler
+    // register server commands in this event handler (Remove if not needed)
+    public void serverStarting(FMLServerStartingEvent event) {
+        proxy.serverStarting(event);
+    }
+}
